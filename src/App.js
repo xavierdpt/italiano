73f855db2c140b160f8325ec564fcc365329d8ca
xavierdpt <xavierdpt>
2021-03-18 13:07:53 +0100

    .

--- src/App.js
+++ src/App.js
@@ -3,6 +3,68 @@ import React from 'react';
 
 const examples = [
   {
+    location: "13",
+    section: "Formes de l'article défini",
+    content: [
+      {
+        it: "Il bambino, i bambini.",
+        fr: "L'enfant, les enfants."
+      },
+      {
+        it: "Lo sbaglio, gli sbagli.",
+        fr: "L'erreur, les erreurs."
+      },
+      {
+        it: "Lo zaino, gli zaini.",
+        fr: "Le sac à dos, les sacs à dos."
+      },
+      {
+        it: "Lo xilofono, gli xilofoni.",
+        fr: "Le xylophone, les xylophones."
+      },
+      {
+        it: "Lo yogurt, gli yogurt.",
+        fr: "Le yaourt, les yaourts."
+      },
+      {
+        it: "Lo pneumatico, gli pneumatici.",
+        fr: "Le pneu, les pneus."
+      },
+      {
+        it: "Lo psicologo, gli psicologi.",
+        fr: "Le psychologue, les psychologues."
+      },
+      {
+        it: "Lo gnocco, gli gnocchi.",
+        fr: "Le gnocchi, les gnocchis."
+      },
+      {
+        it: "Lo iato, gli iati.",
+        fr: "Le hiatus, les hiatus."
+      },
+      {
+        it: "L'albero, gli alberi.",
+        fr: "L'arbre, les arbres."
+      },
+      {
+        it: "La casa, le case.",
+        fr: "La maison, les maisons."
+      },
+      {
+        it: "La iena, le iene.",
+        fr: "La hyène, les hyènes."
+      },
+      {
+        it: "L'isola, le isole.",
+        fr: "L'île, les îles."
+      },
+      {
+        it: "Il dio, gli dèi.",
+        fr: "Le dieu, les dieux."
+      },
+    ]
+  },
+  {
     location: "14",
     section: "Emplois principaux de l'article défini",
     content: [{
@@ -146,21 +208,214 @@ const examples = [
         fr: "D'après les Évangiles, le Christ est né à Bethléem."
       },
     ]
+  },
+  {
+    location: "16",
+    section: "L'article contracté",
+    subsection: "matrice",
+    content: [
+      { it: "del", fr: "di il" },
+      { it: "dello", fr: "di lo" },
+      { it: "dell'", fr: "di l'" },
+      { it: "della", fr: "di la" },
+      { it: "dei", fr: "di i" },
+      { it: "degli", fr: "di gli" },
+      { it: "delle", fr: "di le" },
+      { it: "al", fr: "a il" },
+      { it: "allo", fr: "a lo" },
+      { it: "all'", fr: "a l'" },
+      { it: "alla", fr: "a la" },
+      { it: "ai", fr: "a i" },
+      { it: "agli", fr: "a gli" },
+      { it: "alle", fr: "a le" },
+      { it: "dal", fr: "da il" },
+      { it: "dallo", fr: "da lo" },
+      { it: "dall'", fr: "da l'" },
+      { it: "dalla", fr: "da la" },
+      { it: "dai", fr: "da i" },
+      { it: "dagli", fr: "da gli" },
+      { it: "dalle", fr: "da le" },
+      { it: "nel", fr: "in il" },
+      { it: "nello", fr: "in lo" },
+      { it: "nell'", fr: "in l'" },
+      { it: "nella", fr: "in la" },
+      { it: "nei", fr: "in i" },
+      { it: "negli", fr: "in gli" },
+      { it: "nelle", fr: "in le" },
+      { it: "sul", fr: "su il" },
+      { it: "sullo", fr: "su lo" },
+      { it: "sull'", fr: "su l'" },
+      { it: "sulla", fr: "su la" },
+      { it: "sui", fr: "su i" },
+      { it: "sugli", fr: "su gli" },
+      { it: "sulle", fr: "su le" },
+      { it: "col", fr: "con il" },
+      { it: "coi", fr: "con i" },
+    ]
+  },
+  {
+    location: "16",
+    section: "L'article contracté",
+    subsection: "exemples",
+    content: [
+      {
+        it: "Sono andata al cinema a vedere un documentario sul Neorealismo.",
+        fr: "Je suis allée au cinéma voir un documentaire sur le Néo-réalisme."
+      },
+      {
+        it: "Quando mi hai chiamato ero nello studio dell'avvocato di mio padre.",
+        fr: "Quand tu m'a appelé, j'étais dans le cabinet de l'avocat de mon père."
+      },
+      {
+        it: "Con questo caldo rimarrei in piscina dall'alba al tramonto.",
+        fr: "Avec cette chaleur je resterais dans la piscine de l'aube au [coucher du soleil]."
+      }
+    ]
+  },
+  {
+    location: "16",
+    section: "Fin section 2",
+    subsection: "Exercices 1",
+    content: [
+      {
+        it: "lo zucchero",
+        fr: "? zucchero"
+      },
+      {
+        it: "il tavolo",
+        fr: "? tavolo"
+      },
+      {
+        it: "la sciarpa",
+        fr: "? sciarpa"
+      },
+      {
+        it: "le ragazze",
+        fr: "? ragazze"
+      },
+      {
+        it: "gli psichiatri",
+        fr: "? psichiatri"
+      },
+      {
+        it: "i bambini",
+        fr: "? bambini"
+      },
+      {
+        it: "gli armadi",
+        fr: "? armadi"
+      }
+    ]
+  },
+  {
+    location: "16",
+    section: "Fin section 2",
+    subsection: "Exercices 2",
+    content: [
+      {
+        it: "Il mio treno parte alle sei.",
+        fr: "Mon train part à six heures."
+      },
+      {
+        it: "Devo tornare a casa, è mezzanotte.",
+        fr: "Je dois rentrer à la maison, il est minuit."
+      },
+      {
+        it: "Il 50% del territorio italiano è composto da alta e media montagna.",
+        fr: "50% du territoire italien est composé de haute et moyenne montagne."
+      },
+      {
+        it: "Il signor Landi è uscito.",
+        fr: "Monsieur Landi est sorti."
+      },
+      {
+        it: "Papa Francesco è stato eletto nel 2013.",
+        fr: "Le pape François a été élu en 2013."
+      }
+    ]
+  },
+  {
+    location: "17",
+    section: "Forme de l'article indéfini",
+    content: [
+      {
+        it: "Un ragazzo, dei ragazzi",
+        fr: "Un jeune homme, des jeunes hommes"
+      },
+      {
+        it: "Uno studente, degli studenti",
+        fr: "Un étudiant, des étudiants"
+      },
+      {
+        it: "Uno zerbino, degli zerbini",
+        fr: "Un paillasson, des paillassons"
+      },
+      {
+        it: "Uno xenofobo, degli xenofobi",
+        fr: "Un xénophobe, des xénophobes"
+      },
+      {
+        it: "Uno yacht, degli yacht",
+        fr: "Un yacht, des yachts"
+      },
+      {
+        it: "Uno pneumatico, degli pneumatici",
+        fr: "Un pneu, des pneus"
+      },
+      {
+        it: "Uno pseudonimo, degli pseudonimi",
+        fr: "Un pseudonyme, des pseudonymes"
+      },
+      {
+        it: "Uno gnomo, degli gnomi",
+        fr: "Un gnome, des gnomes"
+      },
+      {
+        it: "Uno iato, degli iati",
+        fr: "Un hiatus, des hiatus"
+      },
+      {
+        it: "Una cantante, delle cantanti",
+        fr: "Une chanteuse, des chanteuses"
+      },
+      {
+        it: "Un'albicocca, delle albicocche",
+        fr: "Un abricot, des abricots"
+      },
+      {
+        it: "",
+        fr: ""
+      },
+      {
+        it: "",
+        fr: ""
+      },
+      {
+        it: "",
+        fr: ""
+      },
+      {
+        it: "",
+        fr: ""
+      },
+    ]
   }
 ];
 
 class App extends React.Component {
   constructor(props) {
     super(props);
+    const sections = examples.map(s => false);
+    sections[sections.length - 1] = true;
     this.state = {
       previous: [],
       example: 0,
-      section: 0,
+      section: sections.indexOf(true),
       value: '',
       bad: null,
       congrats: false,
-      score: -(examples[0].content[0].it).length,
-      sections: examples.map(s => true)
+      score: -(examples[sections.indexOf(true)].content[0].it).length,
+      sections
     };
   }
   handleTextChange = (e) => {
@@ -183,11 +438,13 @@ class App extends React.Component {
     }
   }
   next = () => {
-    const { example, section, previous } = this.state;
+    const { example, section, previous, sections } = this.state;
     const next_example = (example + 1) % examples[section].content.length;
     let next_section = section;
     if (next_example == 0) {
-      next_section = (section + 1) % examples.length;
+      do {
+        next_section = (next_section + 1) % examples.length;
+      } while (!sections[next_section])
     }
     this.setState({
       example: next_example,
