fdc5038eaea5fa13992f4bc0491a9c5a30993fa0
xavierdpt <xavierdpt>
2021-03-21 08:47:23 +0100

    .

--- src/App.js
+++ src/App.js
@@ -381,22 +381,808 @@ const examples = [
       {
         it: "Un'albicocca, delle albicocche",
         fr: "Un abricot, des abricots"
+      }
+    ]
+  },
+  {
+    location: "18",
+    section: "Emplois principaux de l'article indéfini",
+    content: [
+      {
+        it: "Siamo andati a trovare un'amica che si è appena sposata.",
+        fr: "Nous sommes allés voir une amie qui vient de se marier."
       },
       {
-        it: "",
-        fr: ""
+        it: "Mi è stato regalato un libro che avevo già letto.",
+        fr: "On m'a offert un livre que j'avais déjà lu."
+      }, {
+        it: "Un bambino ha bisogno della mamma.",
+        fr: "Un enfant a besoin de sa mère."
+      }, {
+        it: "Abbiamo degli ospiti a cena.",
+        fr: "Nous avons des invités à dîner."
+      }, {
+        it: "Abbiamo ospiti a cena.",
+        fr: "Nous avons [des] invités à dîner."
+      },
+    ]
+  },
+  {
+    location: "18",
+    section: "Formes et emplois de l'article partitif",
+    content: [
+      {
+        it: "Vuoi dello zucchero nel caffè?",
+        fr: "Veux-tu du sucre dans ton café?"
       },
       {
-        it: "",
-        fr: ""
+        it: "Abbiamo ricevuto delle informazioni importanti.",
+        fr: "Nous avons reçu des informations importantes."
+      }
+    ]
+  },
+  {
+    location: "19",
+    section: "Omission de l'article partitif",
+    content: [
+      {
+        it: "I suoi genitori sono persone note in cità.",
+        fr: "Ses parents sont des personnes connues dans la ville."
       },
       {
-        it: "",
-        fr: ""
+        it: "Ho comprato [del] burro e [della] marmellata.",
+        fr: "J'ai acheté [[du]] beurre et [[de la]] confiture."
       },
       {
-        it: "",
-        fr: ""
+        it: "Vorrei un po' di pane.",
+        fr: "Je voudrais un peu de pain"
+      },
+      {
+        it: "Quella ragazza ha un certo stile.",
+        fr: "Cette fille a un certain style."
+      },
+      {
+        it: "Dammi l'acqua, per favore.",
+        fr: "Donne-moi l'eau, s'il te plaît."
+      },
+      {
+        it: "Vuoi la pizza o gli spaghetti?",
+        fr: "Veux-tu la pizza ou les spaghettis?"
+      },
+      {
+        it: "Sandro ha telefonato ad alcuni amici per invitari al suo compleanno.",
+        fr: "Sandro a téléphoné à des amis pour les inviter à son anniversaire."
+      },
+      {
+        it: "È necessario diluire il prodotto in un po' di acqua.",
+        fr: "Il est nécessaire de diluer le produit dans un peu d'eau."
+      },
+      {
+        it: "Oggi non ho ricevuto posta.",
+        fr: "Aujourd'hui je n'ai pas reçu de courrier."
+      },
+      {
+        it: "Non voglio vino.",
+        fr: "Je ne veux pas de vin."
+      }
+    ]
+  },
+  {
+    location: "20",
+    section: "Les articles indéfinis et partitifs",
+    subsection: "Exercices",
+    content: [
+      {
+        it: "Preferisci la carne o il pesce?",
+        fr: "Tu préfères de la viande ou du poisson?"
+      },
+      {
+        it: "Non posso fare foto, perché ho dimenticato la macchina fotographica.",
+        fr: "Je ne peux pas prendre de photos, parce que j'ai oublié mon appareil photo."
+      },
+      {
+        it: "Bisogna mettere la pasta in acqua salata.",
+        fr: "Il faut mettre les pâtes dans de l'eau salée."
+      },
+      {
+        it: "Non voglio zucchero nel caffè.",
+        fr: "Je ne veux pas de sucre dans mon café."
+      },
+      {
+        it: "Vuoi birra o vino?",
+        fr: "Veux-tu de la bière ou du vin?"
+      },
+      {
+        it: "Ho comprato pomodori, mele e spinaci.",
+        fr: "J'ai acheté des tomates, des pommes et des épinards."
+      },
+      {
+        it: "Non beve mai tisane la sera.",
+        fr: "Il ne boit jamais de tisane le soir."
+      },
+    ]
+  },
+  {
+    location: "21",
+    section: "Genre des noms désignant des êtres animés",
+    subsection: "La terminaison dépend du genre",
+    content: [
+      {
+        it: "il ragazzo",
+        fr: "le jeune homme"
+      },
+      {
+        it: "la ragazza",
+        fr: "la jeune femme"
+      },
+      {
+        it: "l'alunno",
+        fr: "l'élève (il)"
+      },
+      {
+        it: "l'alunna",
+        fr: "l'elève (elle)"
+      },
+      {
+        it: "l'amico",
+        fr: "l'ami"
+      },
+      {
+        it: "l'amica",
+        fr: "l'amie"
+      },
+      {
+        it: "il maestro",
+        fr: "l'instituteur"
+      },
+      {
+        it: "la maestra",
+        fr: "l'institutrice"
+      },
+      {
+        it: "il cameriere",
+        fr: "le serveur"
+      },
+      {
+        it: "la cameriera",
+        fr: "la serveuse"
+      },
+      {
+        it: "l'infermiere",
+        fr: "l'infirmier"
+      },
+      {
+        it: "l'infermiera",
+        fr: "l'infirmière"
+      },
+      {
+        it: "il signore",
+        fr: "le monsieur"
+      },
+      {
+        it: "la signora",
+        fr: "la dame"
+      },
+      {
+        it: "il poeta",
+        fr: "le poète"
+      },
+      {
+        it: "la poetessa",
+        fr: "la poète"
+      },
+      {
+        it: "il leone",
+        fr: "le lion"
+      },
+      {
+        it: "la leonessa",
+        fr: "la lionne"
+      },
+      {
+        it: "lo studente",
+        fr: "l'étudiant"
+      },
+      {
+        it: "la studentessa",
+        fr: "l'étudiante"
+      },
+      {
+        it: "l'attore",
+        fr: "l'acteur"
+      },
+      {
+        it: "l'attrice",
+        fr: "l'actice"
+      },
+      {
+        it: "lo scrittore",
+        fr: "l'écrivain"
+      },
+      {
+        it: "la scrittrice",
+        fr: "l'écrivaine"
+      },
+      {
+        it: "il dottore",
+        fr: "le médecin"
+      },
+      {
+        it: "la dottoressa",
+        fr: "la médecin"
+      },
+    ]
+  },
+  {
+    location: "21",
+    section: "Genre des noms désignant des êtres animés",
+    subsection: "La terminaison est identique",
+    content: [
+      {
+        it: "l'artista",
+        fr: "l'artiste"
+      },
+      {
+        it: "il farmacista, la farmacista",
+        fr: "le pharmacien, la pharmacienne"
+      },
+      {
+        it: "il giornalista, la giornalista",
+        fr: "le journaliste, la journaliste"
+      },
+      {
+        it: "il cliente, la cliente",
+        fr: "le client, la cliente"
+      },
+      {
+        it: "il francese, la francese",
+        fr: "le français, la française"
+      },
+      {
+        it: "il nipote, la nipote",
+        fr: "le neveu, la nièce"
+      },
+      {
+        it: "il nipote, la nipote",
+        fr: "le petit-fils, la petite-fille"
+      },
+      {
+        it: "l'atleta",
+        fr: "l'athlète"
+      },
+      {
+        it: "il collega, la collega",
+        fr: "le collègue, la collègue"
+      },
+      {
+        it: "l'ipocrita",
+        fr: "l'hypocrite"
+      },
+      {
+        it: "l'omicida, l'omicida",
+        fr: "le meurtrier, la meurtrière"
+      },
+      {
+        it: "lo psichiatra, la psichiatra",
+        fr: "le psychiatre, la psychiatre"
+      },
+      {
+        it: "l'agente",
+        fr: "l'agent"
+      },
+      {
+        it: "il cantante, la cantante",
+        fr: "le chanteur, la chanteuse"
+      },
+      {
+        it: "l'insegnante, l'insegnante",
+        fr: "l'enseignant, l'enseignante"
+      },
+      {
+        it: "l'amante, l'amante",
+        fr: "l'amant, la maîtresse"
+      },
+      {
+        it: "agire",
+        fr: "agir"
+      },
+      {
+        it: "cantare",
+        fr: "chanter"
+      },
+      {
+        it: "insegnare",
+        fr: "enseigner"
+      },
+      {
+        it: "amare",
+        fr: "aimer"
+      },
+    ]
+  },
+  {
+    location: "22",
+    section: "Genre des noms désignant des êtres animés",
+    subsection: "Le masculin et le féminin sont indépendants",
+    content: [
+      {
+        it: "l'uomo, la donna",
+        fr: "l'homme, la femme"
+      },
+      {
+        it: "il padre, la madre",
+        fr: "le père, la mère"
+      },
+      {
+        it: "il fratello, la sorella",
+        fr: "le frère, la soeur"
+      },
+      {
+        it: "il marito, la moglie",
+        fr: "le mari, la femme"
+      },
+      {
+        it: "il re, la regina",
+        fr: "le roi, la reine"
+      },
+      {
+        it: "il dio, la dea",
+        fr: "le dieu, la déesse"
+      },
+      {
+        it: "il boia",
+        fr: "le bourreau"
+      },
+      {
+        it: "l'ostetrica",
+        fr: "la sage-femme"
+      }
+    ]
+  },
+  {
+    location: "22",
+    section: "Genre des noms désignant des entités inanimées",
+    content: [
+      {
+        it: "l'albero",
+        fr: "l'arbre"
+      },
+      {
+        it: "il libro",
+        fr: "le livre"
+      },
+      {
+        it: "il prezzo",
+        fr: "le prix"
+      },
+      {
+        it: "il dente",
+        fr: "la dent"
+      },
+      {
+        it: "il maglione",
+        fr: "le pull"
+      },
+      {
+        it: "il sole",
+        fr: "le soleil"
+      },
+      {
+        it: "il mare",
+        fr: "la mer"
+      },
+      {
+        it: "il clima",
+        fr: "le climat"
+      },
+      {
+        it: "il problema",
+        fr: "le problème"
+      },
+      {
+        it: "il programma",
+        fr: "le programme"
+      },
+      {
+        it: "lo schema",
+        fr: "le schéma"
+      },
+      {
+        it: "il sistema",
+        fr: "le système"
+      },
+      {
+        it: "l'amore",
+        fr: "l'amour"
+      },
+      {
+        it: "il colore",
+        fr: "la couleur"
+      },
+      {
+        it: "il dolore",
+        fr: "la douleur"
+      },
+      {
+        it: "il fiore",
+        fr: "la fleur"
+      },
+      {
+        it: "la casa",
+        fr: "la maison"
+      },
+      {
+        it: "la mela",
+        fr: "la pomme"
+      },
+      {
+        it: "la penna",
+        fr: "le stylo"
+      },
+      {
+        it: "l'emozione",
+        fr: "l'émotion"
+      },
+      {
+        it: "la fine",
+        fr: "la fin"
+      },
+      {
+        it: "la stazione",
+        fr: "la gare"
+      },
+      {
+        it: "l'analisi",
+        fr: "l'analyse"
+      },
+      {
+        it: "la crisi",
+        fr: "la crise"
+      },
+      {
+        it: "l'oasi",
+        fr: "l'oasis"
+      },
+      {
+        it: "la parentesi",
+        fr: "la parenthèse"
+      },
+      {
+        it: "la bontà",
+        fr: "la bonté"
+      },
+      {
+        it: "la libertà",
+        fr: "la liberté"
+      },
+      {
+        it: "la gioventù",
+        fr: "la jeunesse"
+      },
+      {
+        it: "la virtù",
+        fr: "la vertu"
+      },
+      {
+        it: "la mano",
+        fr: "la main"
+      },
+      {
+        it: "l'eco",
+        fr: "l'écho"
+      },
+      {
+        it: "l'auto",
+        fr: "l'auto"
+      },
+      {
+        it: "la foto",
+        fr: "la photo"
+      },
+      {
+        it: "la moto",
+        fr: "la moto"
+      },
+      {
+        it: "la radio",
+        fr: "la radio"
+      },
+      {
+        it: "l'alibi",
+        fr: "l'alibi"
+      },
+      {
+        it: "il brindisi",
+        fr: "le toast"
+      },
+      {
+        it: "il kiwi",
+        fr: "le kiwi"
+      },
+      {
+        it: "lo sci",
+        fr: "le ski"
+      }
+    ]
+  },
+  {
+    location: "23",
+    section: "Différences de genre entre l'italien et le français",
+    content: [
+      {
+        it: "il colore",
+        fr: "la couleur"
+      },
+      {
+        it: "il dolore",
+        fr: "la douleur"
+      },
+      {
+        it: "l'errore",
+        fr: "l'erreur"
+      },
+      {
+        it: "il fiore",
+        fr: "la fleur"
+      },
+      {
+        it: "il valore",
+        fr: "la valeur"
+      },
+      {
+        it: "la folgore",
+        fr: "la foudre"
+      },
+      {
+        it: "l'affetto",
+        fr: "l'affection"
+      },
+      {
+        it: "l'annullamento",
+        fr: "l'annulation"
+      },
+      {
+        it: "l'aumento",
+        fr: "l'augmentation"
+      },
+      {
+        it: "il consumo",
+        fr: "la consommation"
+      },
+      {
+        it: "l'inquinamento",
+        fr: "la pollution"
+      },
+      {
+        it: "l'intervento",
+        fr: "l'intervention"
+      },
+      {
+        it: "l'invito",
+        fr: "l'invitation"
+      },
+      {
+        it: "l'obbligo",
+        fr: "l'obligation"
+      },
+      {
+        it: "l'Adriatico",
+        fr: "l'Adriatique"
+      },
+      {
+        it: "il Mar Rosso",
+        fr: "la mer Rouge"
+      },
+      {
+        it: "il Mediterraneo",
+        fr: "la Méditerranée"
+      },
+      {
+        it: "l'aiuto",
+        fr: "l'aide"
+      },
+      {
+        it: "il dato",
+        fr: "la donnée"
+      },
+      {
+        it: "il dente",
+        fr: "la dent"
+      },
+      {
+        it: "il foglio",
+        fr: "la feuille"
+      },
+      {
+        it: "l'indirizzo",
+        fr: "l'adresse"
+      },
+      {
+        it: "il limite",
+        fr: "la limite"
+      },
+      {
+        it: "il margine",
+        fr: "la marge"
+      },
+      {
+        it: "il metodo",
+        fr: "la méthode"
+      },
+      {
+        it: "il minuto",
+        fr: "la minute"
+      },
+      {
+        it: "l'olio",
+        fr: "l'huile"
+      },
+      {
+        it: "il paragone",
+        fr: "la comparaison"
+      },
+      {
+        it: "il periodo",
+        fr: "la période"
+      },
+      {
+        it: "il secondo",
+        fr: "la seconde"
+      },
+      {
+        it: "il tentativo",
+        fr: "la tentative"
+      },
+      {
+        it: "la vecchia Firenze",
+        fr: "le vieux Florence"
+      },
+      {
+        it: "la Parigi romantica",
+        fr: "le Paris romantique"
+      },
+      {
+        it: "la vecchia Napoli",
+        fr: "le vieux Naples"
+      },
+      {
+        it: "il Cairo",
+        fr: "le Caire"
+      },
+      {
+        it: "l'agenda",
+        fr: "l'agenda"
+      },
+      {
+        it: "l'arte",
+        fr: "l'art"
+      },
+      {
+        it: "la calma",
+        fr: "le calme"
+      },
+      {
+        it: "la caramella",
+        fr: "le bonbon"
+      },
+      {
+        it: "la cifra",
+        fr: "le chiffre"
+      },
+      {
+        it: "la coppia",
+        fr: "le couple"
+      },
+      {
+        it: "la domenica",
+        fr: "le dimanche"
+      },
+      {
+        it: "l'estate",
+        fr: "l'été"
+      },
+      {
+        it: "l'età",
+        fr: "l'âge"
+      },
+      {
+        it: "la fronte",
+        fr: "le front (du visage)"
+      },
+      {
+        it: "la guida",
+        fr: "le guide"
+      },
+      {
+        it: "la nuvola",
+        fr: "le nuage"
+      },
+      {
+        it: "l'orchestra",
+        fr: "l'orchestre"
+      },
+      {
+        it: "la percentuale",
+        fr: "le pourcentage"
+      },
+      {
+        it: "la plastica",
+        fr: "le plastique"
+      },
+      {
+        it: "la primavera",
+        fr: "le printemps"
+      },
+      {
+        it: "la sabbia",
+        fr: "le sable"
+      },
+      {
+        it: "la tariffa",
+        fr: "le tarif"
+      },
+      {
+        it: "l'unghia",
+        fr: "l'ongle"
+      },
+    ]
+  },
+  {
+    location: "23",
+    section: "Le genre des noms",
+    subsection: "Exercice",
+    content: [
+      {
+        it: "la contessa",
+        fr: "\"il conte\" au féminin"
+      },
+      {
+        it: "la professoressa",
+        fr: "\"il professore\" au féminin"
+      },
+      {
+        it: "la moglie",
+        fr: "\"il marito\" au féminin"
+      },
+      {
+        it: "la zia",
+        fr: "\"lo zio\" au féminin"
+      },
+      {
+        it: "la marchesa",
+        fr: "\"il marchese\" au féminin"
+      },
+      {
+        it: "la leonessa",
+        fr: "\"il leone\" au féminin"
+      },
+      {
+        it: "la traditrice",
+        fr: "\"il traditore\" au féminin"
+      },
+      {
+        it: "la pessimista",
+        fr: "\"il pessimista\" au féminin"
+      },
+      {
+        it: "il colore",
+        fr: "? colore"
+      },
+      {
+        it: "il mare",
+        fr: "? mare"
+      },
+      {
+        it: "la stagione",
+        fr: "? stagione"
+      },
+      {
+        it: "la povertà",
+        fr: "? povertà"
+      },
+      {
+        it: "il fiore",
+        fr: "? fiore"
       },
     ]
   }
@@ -441,7 +1227,7 @@ class App extends React.Component {
     const { example, section, previous, sections } = this.state;
     const next_example = (example + 1) % examples[section].content.length;
     let next_section = section;
-    if (next_example == 0) {
+    if (next_example === 0) {
       do {
         next_section = (next_section + 1) % examples.length;
       } while (!sections[next_section])
